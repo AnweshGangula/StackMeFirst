0697db09e2907c6586644245a23ee11ed78ea15d
Gangula2 <gangulaanwesh@gmail.com>
2022-07-31 02:06:08 +0530

    restore_options on every popup #1

--- Manifest_v2/popup.js
+++ Manifest_v2/popup.js
@@ -5,18 +5,22 @@ let defaultOptions = {
 }
 
 async function displayHTML() {
+    restore_options();
+
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
         // get current Tab - https://stackoverflow.com/a/29151677/6908282
         let activeTab = tabs[0];
         let activeURL = new URL(activeTab.url)
         let currURL = activeURL.href // .at(-1)
         let website = activeURL.host;
-        console.log(activeURL);
-        if (website == "stackoverflow.com") {
-            document.getElementById("notification").style.display = "none";
-        }
-        else {
-            document.getElementById("notification").style.display = "block";
+        let URLpathname = activeURL.pathname;
+        chrome.browserAction.getBadgeText({ tabId: activeTab.id }, badgeText => {
+            // https://stackoverflow.com/a/73178480/6908282
+            if (badgeText == "" || badgeText == "0A0C") {
+                document.getElementById("notification").style.display = "block"
+            }
+        });
+        if (website != "stackoverflow.com") {
             document.getElementById("config").style.display = "none";
         }
     });
@@ -39,12 +43,7 @@ async function save_options() {
         highlightLinkedQues: hlLinkedQs,
     }
     chrome.storage.sync.set({ stackMeData: stackMeData }, function () {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
+        UpdateStatus("Options Saved.");
     });
 }
 
@@ -66,11 +65,16 @@ async function reset_options() {
         document.getElementById('hlComments').value = defaultOptions.highlightLinkedQues;
     });
 
+    UpdateStatus("Options reset.");
+    restore_options;
+}
+
+function UpdateStatus(statusText) {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options reset.';
+    status.textContent = statusText;
+    status.style.visibility = "visible";
     setTimeout(function () {
-        status.textContent = '';
+        status.style.visibility = "hidden";
     }, 750);
-    restore_options;
 }
