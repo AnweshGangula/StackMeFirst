646f56c06fd96c49dc58395cbb8a638413ecfbf0
Gangula2 <gangulaanwesh@gmail.com>
2022-09-01 19:14:05 +0530

    Added popup html and script

--- StackMeFirst-Svelte/src/entries/popup/App.svelte
+++ StackMeFirst-Svelte/src/entries/popup/App.svelte
@@ -1,17 +1,10 @@
 <script>
-import PageContent from "~/lib/PageContent.svelte";
+	import PageContent from "~/lib/PageContent.svelte";
 </script>
 
 <main>
-  <PageContent> Popup </PageContent>
+	<PageContent />
 </main>
 
 <style>
-main {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
