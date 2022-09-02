bf933bcecf02e51a01882ceef52280317eb4da30
Gangula2 <gangulaanwesh@gmail.com>
2022-09-02 19:43:39 +0530

    ES6 contentScript - separate file for async loading reference: https://github.com/GanguLabs/chrome-extension-es6-import/blob/master/src/js/content_script.js

--- src/entries/contentScript/primary/main.js
+++ src/entries/contentScript/primary/main.js
@@ -1,4 +1,4 @@
-import highlightStack, { renderContent } from "../contentScript";
+import highlightStack, { renderContent } from "../contentScript_main.js";
 import App from "./App.svelte";
 
 // renderContent(import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS, (appRoot) => {
