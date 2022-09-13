ae8ecd669619d23bb9a7870105a7b9e3afc96b19
Gangula2 <gangulaanwesh@gmail.com>
2022-09-13 17:41:15 +0530

    separated header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,60 @@
+<script>
+	import browser from "webextension-polyfill";
+	import Api from "~/utils/stackAPI";
+
+	let stackAPI;
+	let loggedIn = false;
+	async function login() {
+		// this.setState({ loading: true });
+		alert("Sending Message to backgroundScript to login");
+
+		browser.runtime
+			.sendMessage({
+				from: "popup",
+				subject: "AUTH",
+			})
+			.then(async ({ token, error }) => {
+				console.log(`Action 'AUTH' success`);
+				if (token) {
+					// console.log("Logged in");
+					loggedIn = true;
+					stackAPI = new Api(token);
+					const myData = await stackAPI.getMyDetails();
+					document.getElementById("btnLogout").title = myData[0].display_name;
+
+					const apiData = {
+						token: token,
+						userName: myData[0].display_name,
+					};
+					browser.storage.sync.set({ apiData: apiData }).then(function () {
+						// UpdateStatus("Options Saved");
+					});
+				} else {
+					console.log("Unable to login");
+					// this.setState({ error });
+				}
+			});
+		// return true;
+	}
+</script>
+
+<header>
+	<img id="logo" src="/icons/StackMeFirst.png" alt="Stack Me First Logo" width="20" height="20" />
+	<h1>Stack Me First</h1>
+
+	{#if loggedIn}
+		<button id="btnLogout" class="loginBtn" on:click|preventDefault>Logout</button>
+	{:else}
+		<button id="btnLogin" class="loginBtn" on:click|preventDefault={() => login()} title="Click to Login to Stack Overflow">Login</button>
+	{/if}
+</header>
+
+<style>
+	#logo {
+		margin: 5px;
+	}
+
+	.loginBtn {
+		margin-left: auto;
+	}
+</style>
--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -1,58 +1,12 @@
 <script>
-	import browser from "webextension-polyfill";
-	import Api from "~/utils/stackAPI";
 	import Preferences from "~/lib/Preferences.svelte";
-
-	let stackAPI;
-	let loggedIn = false;
-	async function login() {
-		// this.setState({ loading: true });
-		alert("Sending Message to backgroundScript to login");
-
-		browser.runtime
-			.sendMessage({
-				from: "popup",
-				subject: "AUTH",
-			})
-			.then(async ({ token, error }) => {
-				console.log(`Action 'AUTH' success`);
-				if (token) {
-					// console.log("Logged in");
-					loggedIn = true;
-					stackAPI = new Api(token);
-					const myData = await stackAPI.getMyDetails();
-					document.getElementById("btnUserName").textContent = myData[0].display_name;
-
-					const apiData = {
-						token: token,
-						userName: myData[0].display_name,
-					};
-					browser.storage.sync.set({ apiData: apiData }).then(function () {
-						// UpdateStatus("Options Saved");
-					});
-				} else {
-					console.log("Unable to login");
-					// this.setState({ error });
-				}
-			});
-		// return true;
-	}
+	import Header from "./Header.svelte";
 
 	export let pageType = "popup";
 	export let isStackOverflow = true;
 </script>
 
-<header>
-	<img id="logo" src="/icons/StackMeFirst.png" alt="Stack Me First Logo" width="20" height="20" />
-	<h1>Stack Me First</h1>
-
-	{#if loggedIn}
-		<button id="btnUserName" class="loginBtn" on:click|preventDefault>Logged in</button>
-	{:else}
-		<button id="btnLogin" class="loginBtn" on:click|preventDefault={() => login()}>Login</button>
-	{/if}
-</header>
-
+<Header />
 <main>
 	<slot />
 	{#if pageType == "popup" && isStackOverflow}
@@ -88,11 +42,6 @@
 		display: flex;
 		align-items: center;
 	}
-
-	#logo {
-		margin: 5px;
-	}
-
 	hr {
 		/* reference: https://stackoverflow.com/a/5619906/6908282 */
 		margin-top: 5px;
@@ -105,9 +54,5 @@
 		color: white;
 		padding: 0 5px;
 		text-align: center;
-	}
-
-	.loginBtn {
-		margin-left: auto;
 	}
 </style>
