72a5b0a7fd6557de031e25ba643926e097a07eaf
Gangula2 <gangulaanwesh@gmail.com>
2022-09-13 17:38:28 +0530

    separated preferences component

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -1,6 +1,7 @@
 <script>
 	import browser from "webextension-polyfill";
 	import Api from "~/utils/stackAPI";
+	import Preferences from "~/lib/Preferences.svelte";
 
 	let stackAPI;
 	let loggedIn = false;
@@ -74,34 +75,7 @@
 		</div>
 	{/if}
 
-	<div id="config">
-		<form id="options">
-			<fieldset>
-				<legend>Configure Settings</legend>
-
-				<div>
-					<input type="checkbox" id="hlAnswers" name="hlAnswers" value="Highlight my Answers" />
-					<label for="hlAnswers">Highlight my Answers</label>
-				</div>
-				<div>
-					<input type="checkbox" id="srtAns" name="srtAns" value="Bring My Answers to Top" />
-					<label for="srtAns">Bring My Answers to Top</label><br />
-				</div>
-				<br />
-				<div>
-					<input type="checkbox" id="hlComments" name="hlComments" value="Highlight Comments" />
-					<label for="hlComments">Highlight my Comments</label><br />
-				</div>
-			</fieldset>
-		</form>
-
-		<div id="buttons">
-			<button id="btnSave" type="button">Save</button>
-			<button id="btnReset" type="button">Reset</button>
-		</div>
-
-		<p id="status" />
-	</div>
+	<Preferences />
 </main>
 
 <style>
@@ -115,12 +89,8 @@
 		align-items: center;
 	}
 
-	:global(#logo) {
+	#logo {
 		margin: 5px;
-	}
-
-	input {
-		margin-left: 5px;
 	}
 
 	hr {
@@ -130,20 +100,6 @@
 		border: none;
 		background: lightgray;
 	}
-
-	#buttons {
-		margin: 10px;
-		text-align: end;
-	}
-
-	#status {
-		visibility: hidden;
-		color: green;
-		background-color: darkseagreen;
-		margin: 5px;
-		padding: 5px;
-	}
-
 	.featureOff {
 		background-color: firebrick;
 		color: white;
---
+++ src/lib/Preferences.svelte
@@ -0,0 +1,47 @@
+<div id="config">
+	<form id="options">
+		<fieldset>
+			<legend>User Preferences</legend>
+
+			<div>
+				<input type="checkbox" id="hlAnswers" name="hlAnswers" value="Highlight my Answers" />
+				<label for="hlAnswers">Highlight my Answers</label>
+			</div>
+			<div>
+				<input type="checkbox" id="srtAns" name="srtAns" value="Bring My Answers to Top" />
+				<label for="srtAns">Bring My Answers to Top</label><br />
+			</div>
+			<br />
+			<div>
+				<input type="checkbox" id="hlComments" name="hlComments" value="Highlight Comments" />
+				<label for="hlComments">Highlight my Comments</label><br />
+			</div>
+		</fieldset>
+	</form>
+
+	<div id="buttons">
+		<button id="btnSave" type="button">Save</button>
+		<button id="btnReset" type="button">Reset</button>
+	</div>
+
+	<p id="status" />
+</div>
+
+<style>
+	input {
+		margin-left: 5px;
+	}
+
+	#buttons {
+		margin: 10px;
+		text-align: end;
+	}
+
+	#status {
+		visibility: hidden;
+		color: green;
+		background-color: darkseagreen;
+		margin: 5px;
+		padding: 5px;
+	}
+</style>
