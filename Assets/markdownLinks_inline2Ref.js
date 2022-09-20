536d0ce2ba76548156af2857b4f2427f4f939187
Gangula2 <gangulaanwesh@gmail.com>
2022-09-20 21:18:40 +0530

    regex pattern to match existing inline links - bug

--- Assets/markdownLinks_inline2Ref.js
+++ Assets/markdownLinks_inline2Ref.js
@@ -1,20 +1,22 @@
-// refernce: https://stackoverflow.com/a/30919298/6908282
-// node main.js test.md result.md
+// node Assets/markdownLinks_inline2Ref.js ReadMe.md RefLinks.md
 
-var fs = require('fs')
+import * as fs from 'fs'
 fs.readFile(process.argv[2], 'utf8', function (err, markdown) {
     if (err) {
         return console.log(err);
     }
     var counter = 1;
     var matches = {};
-    var matcher = /\[.*?\]\((.*?)\)/g;
-    while (match = matcher.exec(markdown)) {
+    var matcher = /\[.*?\][\(\[](.*?)[\]\)]/g; // /\[.*?\]\((.*?)\)/g
+    let match = true;
+    while (match) {
         if (!matches[match[1]]) matches[match[1]] = counter++;
+        match = matcher.exec(markdown);
     }
     console.log(matches);
     Object.keys(matches).forEach(function (url) {
-        var r = new RegExp("(\\[.*?\\])\\(" + url + "\\)", "g");
+        // var r = new RegExp("(\\[.*?\\])\\(" + url + "\\)", "g");
+        var r = new RegExp("(\\[.*?\\])[\\(\\[]" + url + "[\\]\\)]", "g");
         markdown = markdown.replace(r, "$1[" + matches[url] + "]");
         markdown += "\n[" + matches[url] + "]: " + url;
     });
