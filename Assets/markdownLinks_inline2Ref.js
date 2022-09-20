ab57518e63d7db2e5ccdb6235896b896c122c3fd
Gangula2 <gangulaanwesh@gmail.com>
2022-09-20 22:15:39 +0530

    successful - convert inline links to ref

--- Assets/markdownLinks_inline2Ref.js
+++ Assets/markdownLinks_inline2Ref.js
@@ -1,28 +1,42 @@
 // node Assets/markdownLinks_inline2Ref.js ReadMe.md RefLinks.md
 
 import * as fs from 'fs'
-fs.readFile(process.argv[2], 'utf8', function (err, markdown) {
+fs.readFile(process.argv[2], 'utf8', function (err, mainMarkdown) {
     if (err) {
         return console.log(err);
     }
+    let newMarkdown = existingRefLinks(mainMarkdown);
+
     var counter = 1;
     var matches = {};
-    var matcher = /\[.*?\][\(\[](.*?)[\]\)]/g; // /\[.*?\]\((.*?)\)/g
-    let match = true;
-    while (match) {
+    var matcher = /\[.*?\]\((.*?)\)/g
+    let match;
+    while (match = matcher.exec(newMarkdown)) {
         if (!matches[match[1]]) matches[match[1]] = counter++;
-        match = matcher.exec(markdown);
     }
     console.log(matches);
     Object.keys(matches).forEach(function (url) {
-        // var r = new RegExp("(\\[.*?\\])\\(" + url + "\\)", "g");
-        var r = new RegExp("(\\[.*?\\])[\\(\\[]" + url + "[\\]\\)]", "g");
-        markdown = markdown.replace(r, "$1[" + matches[url] + "]");
-        markdown += "\n[" + matches[url] + "]: " + url;
+        var r = new RegExp("(\\[.*?\\])\\(" + url + "\\)", "g");
+        newMarkdown = newMarkdown.replace(r, "$1[" + matches[url] + "]");
+        newMarkdown += "\n[" + matches[url] + "]: " + url;
     });
 
-    fs.writeFile(process.argv[3], markdown, 'utf8', function (err) {
+    fs.writeFile(process.argv[3], newMarkdown, 'utf8', function (err) {
         if (err) return console.log(err);
     });
 
 });
+
+function existingRefLinks(markdown) {
+    let refLinks = {}, match;
+    const matcher = /\[(\d)]:\s(.*)/g; // /\[.*?\]\((.*?)\)/g
+    while (match = matcher.exec(markdown)) {
+        if (!refLinks[match[1]]) refLinks[match[1]] = match[2];
+    }
+    markdown = markdown.replaceAll(matcher, "")
+
+    Object.keys(refLinks).forEach(function (int) {
+        markdown = markdown.replace("][" + int + "]", "](" + refLinks[int] + ")");
+    });
+    return markdown
+}
