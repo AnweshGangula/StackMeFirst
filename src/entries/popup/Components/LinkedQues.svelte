f2efa2745d1c8d0c578355c59e0e7d70ab501e54
Gangula2 <gangulaanwesh@gmail.com>
2022-09-27 12:54:53 +0530

    Display error if token not found for linked questions

--- src/entries/popup/Components/LinkedQues.svelte
+++ src/entries/popup/Components/LinkedQues.svelte
@@ -10,6 +10,8 @@
 	let token;
 	let linkedQ = [];
 	let isQ;
+	let warning = "Loading";
+	let loading = warning == "Loading" ? "loading" : "";
 	GetUpvotedLinks();
 
 	async function GetPreferences() {
@@ -35,16 +37,25 @@
 		browser.tabs.query({ active: true, lastFocusedWindow: true }).then(function (tabs) {
 			isQ = IsQuestion(tabs[0].url);
 			if (isQ) {
-				browser.tabs.sendMessage(tabs[0].id, { from: "popup", subject: "popupLinkQs" }).then((info) => {
-					token = info.token;
-					glCurrTab = tabs[0];
-					const allLinkedQs = info.linkedQids;
+				browser.tabs
+					.sendMessage(tabs[0].id, { from: "popup", subject: "popupLinkQs" })
+					.then((info) => {
+						token = info.token;
+						glCurrTab = tabs[0];
+						const allLinkedQs = info.linkedQids;
 
-					allLinkedQs.forEach((ques) => {
-						let suffix = ques.isHidden + ques.isFavorite + ques.isAuthor;
-						linkedQ.push(ques.linkJson.question_id.toString() + suffix);
+						allLinkedQs.forEach((ques) => {
+							let suffix = ques.isHidden + ques.isFavorite + ques.isAuthor;
+							linkedQ.push(ques.linkJson.question_id.toString() + suffix);
+						});
+
+						if (!token) {
+							warning = 'Click the "Login" button above to provide access to linked questions';
+						}
+					})
+					.catch((error) => {
+						warning = "Error in fetching LinkQ data from contentScript:\n" + error.message;
 					});
-				});
 			}
 		});
 	}
@@ -55,7 +66,7 @@
 		{#if token}
 			<StackContent eleList={linkedQ} type="linkq" tab={glCurrTab} />
 		{:else}
-			<p class="featureOff">Click the "Login" button above to provide access to linked questions</p>
+			<p class="featureOff {loading}">{warning}</p>
 		{/if}
 	{/if}
 {/await}
@@ -66,5 +77,32 @@
 		color: white;
 		padding: 0 5px;
 		text-align: center;
+	}
+
+	.loading {
+		pointer-events: none;
+	}
+
+	.loading:after {
+		overflow: hidden;
+		display: inline-block;
+		vertical-align: bottom;
+		-webkit-animation: ellipsis steps(4, end) 900ms infinite;
+		animation: ellipsis steps(4, end) 900ms infinite;
+		content: "\2026";
+		/* ascii code for the ellipsis character */
+		width: 0px;
+	}
+
+	@keyframes ellipsis {
+		to {
+			width: 10px;
+		}
+	}
+
+	@-webkit-keyframes ellipsis {
+		to {
+			width: 10px;
+		}
 	}
 </style>
