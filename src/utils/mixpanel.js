689d3db08663fb8f444a11a7e884b36fa67f53fb
Anwesh Gangula <gangulaanwesh@gmail.com>
2023-08-07 05:10:38 +0530

    switched mixpanel project to production project

--- src/utils/mixpanel.js
+++ src/utils/mixpanel.js
@@ -1,11 +1,15 @@
 // Not working with MS Edge right now
+// mixpanel project link: https://mixpanel.com/project/2794687/app/settings#project/2794687
 
 import mixpanel from 'mixpanel-browser';
+
+const devMixpanel = "3dd982c60bba9559c0f2f428769f59b4";
+const prodMixPanel = "5280502e9fa283137f3707add408d7d2";
 
 export default class SmfMixpanel {
     constructor(token = "", pageView = false) {
         this.token = token;
-        this.token = "3dd982c60bba9559c0f2f428769f59b4";
+        this.token = prodMixPanel; // devMixpanel;
 
         this.init(pageView);
     }
